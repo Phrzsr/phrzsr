fadafc3a264f8e59ae91d063489df242af2f2731
Kam Harrah <kamharrah@gmail.com>
2013-11-26 11:29:26 -0500

    Update Phrzsr.js
    The function is not reaching the “callback test”. I tried replacing the
    anonymous function in httpGet with a basic function that squared a
    number, and that function completed before the “httpGet test” was
    written to the DOM.
    
    Also, there’s a better way to do getRandomPhrase without creating four
    variables, will figure out after the callback issue is fixed.

--- js/phrzsr.js
+++ js/phrzsr.js
@@ -1,1 +1,53 @@
-// Skeleton Phrzsr.js file
+
+
+function phrzsr() {
+	httpGet("https://raw.github.com/Phrzsr/phrzsr/master/motivational", function(response){
+		document.write("callback test");
+		phraseArray = [];
+		phraseArray = response.split("\n");
+		console.log(response);
+
+		
+		document.write(getRandomPhrase(phraseArray));
+	});
+};
+
+function httpGet(url, callback) {
+	document.write("httpGet test");
+	var httpRequest; // create our XMLHttpRequest object
+	if (window.XMLHttpRequest) {
+		httpRequest = new XMLHttpRequest();
+	} else if (window.ActiveXObject) {
+		// Internet Explorer is stupid
+		httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+ 	}
+
+	httpRequest.onreadystatechange = function() {
+		// inline function to check the status
+		// of our request
+		// this is called on every state change
+		if (httpRequest.readyState == 4 &&
+				httpRequest.status == 200) {
+			callback.call(httpRequest.responseText);
+			// call the callback function
+		}
+	};
+	httpRequest.open('GET', url);
+	httpRequest.send();
+};
+
+function getRandomPhrase(phraseArray){
+	phraseLimit  = phraseArray.length - 1;
+	phraseRand 	 = Math.random()*phraseLimit;
+	phraseNumber = Math.round(phraseRand);
+	chosenPhrase = phraseArray[phraseNumber];
+}
+
+phrzsr();
+
+// EXAMPLE HTML
+// <body>
+//		<div id="example_phrase">
+//			<script src="phrzsr.js"></script>  
+//		</div>
+//	</body>
