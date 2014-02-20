aa8d29e5eed8090fdc08032e8e744d2e0dcc8768
Kamerynn Harrah <kamharrah@gmail.com>
2014-02-20 10:18:31 -0500

    Created selectURL func that tolerates misspellings, update html file

--- js/phrzsr.js
+++ js/phrzsr.js
@@ -1,30 +1,35 @@
 // need: auto-complete so any argument that starts with m = motivational, etc.
 var URLs = {
-	motivational: "https://api.github.com/repos/Phrzsr/phrzsr/contents/motivational?callback=grab",
-	demotivational: "https://api.github.com/repos/Phrzsr/phrzsr/contents/demotivational?callback=grab",
-	farewell: "https://api.github.com/repos/Phrzsr/phrzsr/contents/farewell?callback=grab",
-	gratitude: "https://api.github.com/repos/Phrzsr/phrzsr/contents/gratitude?callback=grab",
-	greeting: "https://api.github.com/repos/Phrzsr/phrzsr/contents/greeting?callback=grab"
+	// motivational, demotivational, farewell, gratitude, greeting
+	mot: "https://api.github.com/repos/Phrzsr/phrzsr/contents/motivational?callback=grab",
+	dem: "https://api.github.com/repos/Phrzsr/phrzsr/contents/demotivational?callback=grab",
+	far: "https://api.github.com/repos/Phrzsr/phrzsr/contents/farewell?callback=grab",
+	gra: "https://api.github.com/repos/Phrzsr/phrzsr/contents/gratitude?callback=grab",
+	gre: "https://api.github.com/repos/Phrzsr/phrzsr/contents/greeting?callback=grab"
 }
 
 var pArray = []
 
-function phrzsr(cat) {
+function phrzsr(userCat) {
 	var script = document.createElement('script');
-	if (!cat) {
-		url = URLs['motivational'];
+	if (!userCat) {
+		url = URLs['mot'];
 	} else {
-		category = cat.toLowerCase();
-		if (category in URLs) {
-		url = URLs[category];
-		} else {
-			console.log('Please enter a valid category in the phrzsr function');
-		}
+		url = selectURL(userCat);
 	}
 	script.src = url;
 	document.getElementsByTagName('head')[0].appendChild(script);
 }
 
+function selectURL(category) {
+	var shortURL = category.slice(0, 3).toLowerCase();
+	try {
+		url = URLs[shortURL];
+		return url;
+	} catch (e) {
+		console.log("Phrzsr: please enter a valid category");
+	}
+}
 function grab(response) {
     var b64 = response.data.content.replace(/\n|\r/g, "");
     // issue: sometimes it screws up hyphens, sometimes it adds extra quotes
@@ -38,5 +43,3 @@ function grab(response) {
 function getRandomPhrase(phraseArray){
 	return phraseArray[Math.floor(Math.random() * phraseArray.length)];
 }
-
-phrzsr();
